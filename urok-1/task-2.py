c974814d986c75d3beaa03a8df367b8ef84e6b9c
birnazarova <birnazarova@gmail.com>
2022-10-06 22:37:58 -0700

    homework 3

--- urok-1/task-2.py
+++ urok-1/task-2.py
@@ -1,10 +1,17 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x = int(input('Type a number: '))
-y = int(input('Type a number: '))
-z = int(input('Type a number: '))
+# x = int(input('Type a number: '))
+# y = int(input('Type a number: '))
+# z = int(input('Type a number: '))
 
-if not(x or y or z) == (not x and not y and not z):
-    print('The statement is correct')
-else:
-    print('The statement is not correct')
+# if not(x or y or z) == (not x and not y and not z):
+#     print('The statement is correct')
+# else:
+#     print('The statement is not correct')
+
+### Another solution ###
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(not (x or y or z) == (not x and not y and not z))
