c397c9a4879e40ec1f17d67ca1c6627c8768d2e2
birnazarova <birnazarova@gmail.com>
2022-10-07 23:13:14 -0700

    task 3 done

--- urok-4/task-3.py
+++ urok-4/task-3.py
@@ -1,1 +1,9 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+n = list(map(int, input("type a sequence of numbers: ").split()))
+print(n)
+n2 = []
+for i in n:
+    if n.count(i) <= 1:
+        n2.append(i)
+print(n2)
