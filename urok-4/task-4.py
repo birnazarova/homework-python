882479ad5d3b4bbc46ec0ade3bd0bb6d874e2f31
birnazarova <birnazarova@gmail.com>
2022-10-09 22:01:59 -0700

    h/w 4 done

--- urok-4/task-4.py
+++ urok-4/task-4.py
@@ -5,3 +5,10 @@
 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
+import random
+
+k = input("Задайте степень: ")
+result = str(random.randint(0,101))+'x**' + k + ' + ' + str(random.randint(0,101))+'x + '+str(random.randint(0,101)) + ' = 0'
+
+with open ('/Users/aika/Desktop/Git/python/homework-python/urok-4/file4.txt', 'w') as file:
+    file.write(result)
